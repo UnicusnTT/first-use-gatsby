436741ab5e67c43946dbfc23ae497bf8d5fb9980
TrygvUnicus <trygve.taranger@unicus.com>
2024-04-12 15:45:37 +0200

    Finished part 5 with some minor changes

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -5,11 +5,16 @@ import Seo from '../components/seo'
 import { HeadFC, PageProps, graphql } from 'gatsby'
 
 type nodeObject = {
-    name: string
+    frontmatter: {
+        date: string;
+        title: string;
+    },
+    id: string;
+    excerpt: string;
 }
 
 type DataProps = {
-    allFile: {
+    allMdx: {
       nodes: nodeObject[]
     }
   }
@@ -19,27 +24,32 @@ const title = "Posts"
 const BlogPage: React.FC<PageProps<DataProps>> = ({ data }) => {
     return (
         <Layout pageTitle={title}>
-            <ul>
-                {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    ))
-                }
-            </ul>
+            {
+                data.allMdx.nodes.map(node => (
+                    <article key={node.id}>
+                        <h2>{node.frontmatter.title}</h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                        <p>{node.excerpt}</p>
+                    </article>
+                ))
+            }
         </Layout>
     )
 }
 
 export const query = graphql`
-  query {
-    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
-      nodes {
-        name  
+  query MyQuery {
+  allMdx(sort: { frontmatter: { date: ASC } }) {
+    nodes {
+      frontmatter {
+        date(formatString: "MMMM D, YYYY")
+        title
       }
+      id
+      excerpt
     }
   }
+}
 `
 
 export const Head: HeadFC = () => <Seo title={title} />
